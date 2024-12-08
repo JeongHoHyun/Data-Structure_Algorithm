7749cfb480dd50694108074be02c28051a573a15
happyjeong97 <happyjeong97@naver.com>
2024-12-09 00:12:06 +0900

    코딩테스트 공부

---
+++ src/coding_test/_07Section/_09_DFS_Tree.java
@@ -0,0 +1,23 @@
+package coding_test._07Section;
+class Main9{
+    Node root;
+    public int DFS(int L, Node root) {
+        if (root.lt == null && root.rt == null) {
+            return L;
+        } else {
+            return Math.min(DFS(L + 1, root.lt), DFS(L + 1, root.rt));
+        }
+    }
+}
+
+public class _09_DFS_Tree {
+    public static void main(String[] args) {
+        Main9 tree = new Main9();
+        tree.root = new Node(1);
+        tree.root.lt = new Node(2);
+        tree.root.rt = new Node(3);
+        tree.root.lt.lt = new Node(4);
+        tree.root.lt.rt = new Node(5);
+        System.out.println(tree.DFS(0, tree.root));
+    }
+}
