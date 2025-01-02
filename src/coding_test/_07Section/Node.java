b0828cd18b33ea17e774f051bdc7de6b48528e18
happyjeong97 <happyjeong97@naver.com>
2025-01-02 20:09:46 +0900

    코딩테스트 공부

---
+++ src/coding_test/_07Section/Node.java
@@ -0,0 +1,12 @@
+package coding_test._07Section;
+
+public class Node {
+    int data;
+    Node lt;
+    Node rt;
+
+    public Node(int val) {
+        data = val;
+        lt = rt = null;
+    }
+}
