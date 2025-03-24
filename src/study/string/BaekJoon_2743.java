c15e370d60f479997d7429f462ff16bde82d4b0f
happyjeong97 <happyjeong97@naver.com>
2025-03-24 21:49:25 +0900

    자료구조, 알고리즘

---
+++ src/study/string/BaekJoon_2743.java
@@ -0,0 +1,11 @@
+package study.string;
+
+import java.util.Scanner;
+
+public class BaekJoon_2743 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        System.out.println(s.length());
+    }
+}
