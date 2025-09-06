804a4da3c943814be7040e280b02ca3591678828
happyjeong97 <happyjeong97@naver.com>
2025-09-06 16:09:41 +0900

    코딩테스트 공부

---
+++ src/study/string/BaekJoon_11654.java
@@ -0,0 +1,12 @@
+package study.string;
+
+import java.util.Scanner;
+
+public class BaekJoon_11654 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String S = sc.next();
+        int result = S.charAt(0);
+        System.out.println(result);
+    }
+}
